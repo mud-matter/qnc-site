3cb4410d8323588ccb1faec8cfa214483571e55f
yesimzaim <yesimzaim@gmail.com>
2023-11-03 14:22:03 +0100

    fixed up

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-});
